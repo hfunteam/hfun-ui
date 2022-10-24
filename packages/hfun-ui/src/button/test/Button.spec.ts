e159ca48702828cbb1d47aced307576fde46e574
yeyanghui0616 <69786688+yeyanghui0616@users.noreply.github.com>
2022-10-24 17:13:04 +0800

    test: 修改test目录

--- packages/hfun-ui/src/button/test/Button.spec.ts
+++
@@ -1,7 +0,0 @@
-import { describe, expect, test } from "vitest";
-// 测试分组
-describe("Button", () => {
-  test("Button", () => {
-    expect(1 + 1).toBe(2);
-  });
-});
--- packages/hfun-ui/test/Button.spec.ts
+++ packages/hfun-ui/test/Button.spec.ts
@@ -1,7 +1,7 @@
 import { describe, expect, test } from "vitest";
 // 测试分组
 describe("Button", () => {
-  test("Button", () => {
+  test("Button test", () => {
     expect(1 + 1).toBe(2);
   });
 });
